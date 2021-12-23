4e4d8243b625c61b7c1330f2d9f453e5d9e0852a
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-12-23 17:42:18 -0500

    working on user prompts

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-// const generate = require('./lib/Employee');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-// new generate().initializeApp();
+const employees = [];
+
+const userPrompts =() => {
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Enter the name of the Manager:',
+        name: 'name',
+        validate: userInput => {
+            if(userInput) {
+                return true;
+            } else {
+                console.log('Invalid entry, please try again.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Enter employee Id:',
+        name: 'id',
+        validate: userInput => {
+            if(isNaN(userInput)) {
+                console.log('Invalid entry, please try again.');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Enter employees email address:',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Enter office number:',
+        name: 'officeNumber',
+        validate: userInput => {
+            if (isNaN(userInput)) {
+                console.log('Invalid entry, please try again.');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }])
+    .then(managerInfo => {
+        const {name, id, email, officeNumber} = managerInfo;
+        const manager = new Manager(name, id, email, officeNumber);
+        employees.push(manager);
+    })
+};
